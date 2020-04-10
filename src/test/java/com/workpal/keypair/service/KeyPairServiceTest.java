ad4df57d6b97a340902eacd66d4314905651eaac
Anudeep Kulkarni <anudeepkulkarni111@gmail.com>
2020-04-10 19:01:59 +0530

    Repository mock is done

--- src/test/java/com/workpal/keypair/service/KeyPairServiceTest.java
+++ src/test/java/com/workpal/keypair/service/KeyPairServiceTest.java
@@ -1,19 +1,27 @@
 package com.workpal.keypair.service;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.workpal.keypair.domain.KeyPair;
+import com.workpal.keypair.repository.KeyPairRepository;
 import com.workpal.keypair.request.GenerateKeyPairRequest;
 import com.workpal.keypair.service.impl.KeyPairServiceImpl;
 
 @SpringBootTest
 public class KeyPairServiceTest {
+
+	@Mock
+	private KeyPairRepository keyPairRepo;
 
 	@InjectMocks
 	private KeyPairService keyPairService = new KeyPairServiceImpl();
@@ -22,14 +30,16 @@ public class KeyPairServiceTest {
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@DisplayName("testGenerateKeyPair")
 	@Test
 	public void testGenerateKeyPair() {
 		var generateKeyPairRequest = new GenerateKeyPairRequest();
 		generateKeyPairRequest.setName("Generate key pair test");
 		generateKeyPairRequest.setDescription("Generate key pair test description");
+		when(keyPairRepo.save((any(KeyPair.class)))).thenReturn(new KeyPair());
 		var privateKey = keyPairService.generateKeyPair(generateKeyPairRequest);
-		
+		assertTrue(!privateKey.isEmpty());
+
 	}
 }

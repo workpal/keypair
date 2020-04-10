91c6a0cfb24b6ac24af212dc08afd3dd2768201e
Ramadhas <Ramadhas>
2020-04-10 14:30:26 +0530

    Base project with necassary depedencies

---
+++ src/main/java/com/workpal/keypair/KeypairApplication.java
@@ -0,0 +1,13 @@
+package com.workpal.keypair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeypairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeypairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workpal/keypair/KeypairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workpal.keypair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeypairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

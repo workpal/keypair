bd4271a5d99f4145f7b38ceae6487f75e0537018
Uthirapathi Devendran <uthirapathi.devendran@sifycorp.com>
2020-04-12 15:04:21 +0530

    Remove key pair unit test case

--- src/test/java/com/workpal/keypair/service/KeyPairServiceTest.java
+++ src/test/java/com/workpal/keypair/service/KeyPairServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
